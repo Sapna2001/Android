8c64261ab80a0e397e7d6270184643abdf115684
Sapna2001 <harisapnanair@gmail.com>
2020-09-08 22:47:28 +0530

    Files added

---
+++ app/src/main/java/com/example/birthdaygreeting/BirthdayGreetingActivity.kt
@@ -0,0 +1,24 @@
+package com.example.birthdaygreeting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+//        createBirthdayButton.setOnClickListener {
+//            Toast.makeText(this, "name",Toast.LENGTH_LONG).show()
+//        }
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name = nameInput.editableText.toString()
+        Toast.makeText(this, "$name",Toast.LENGTH_LONG).show()
+    }
+}
+
