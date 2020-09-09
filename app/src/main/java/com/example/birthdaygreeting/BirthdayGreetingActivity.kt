98dcda8a45a51e8cc31e5cd20a46eb8dbb3628f0
Sapna2001 <harisapnanair@gmail.com>
2020-09-10 00:20:20 +0530

    Proj4

--- app/src/main/java/com/example/birthdaygreeting/BirthdayGreetingActivity.kt
+++ app/src/main/java/com/example/birthdaygreeting/BirthdayGreetingActivity.kt
@@ -4,22 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_birthday_greeting.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class BirthdayGreetingActivity : AppCompatActivity() {
+
+    companion object {
+        const val NAME_EXTRA = "name_extra"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_birthday_greeting)
 
-//        createBirthdayButton.setOnClickListener {
-//            val name = nameInput.editableText.toString()
-//             Toast.makeText(this, "$name",Toast.LENGTH_LONG).show()
-//        }
-    }
-
-    fun createBirthdayCard(view: View) {
-        val name = nameInput.editableText.toString()
-        Toast.makeText(this, "$name",Toast.LENGTH_LONG).show()
+        val name = intent.getStringExtra(NAME_EXTRA)
+        birthdayGreeting.text = "Happy Birthday \n $name !!!"
     }
 }
 
--- app/src/main/java/com/example/birthdaygreeting/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreeting/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.birthdaygreeting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun createBirthdayCard(view: View) {}
+    fun createBirthdayCard(view: View) {
+          val name = nameInput.editableText.toString()
+//        Toast.makeText(this,"$name",Toast.LENGTH_LONG).show()
+          val intent = Intent(this,BirthdayGreetingActivity::class.java) //go from one process to another
+          intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA,name)
+          startActivity(intent)
+    }
 }
